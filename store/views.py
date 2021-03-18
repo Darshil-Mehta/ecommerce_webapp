4ad7178eed8d475c3e5e0c454d0c219fccd42b9e
Darshil Mehta <darshilm223@gmail.com>
2021-03-18 12:03:01 +0530

    add to cart feature added

--- store/views.py
+++ store/views.py
@@ -10,18 +10,43 @@ from django.contrib.auth.hashers import make_password, check_password
 # -cid- gets the current category id and calls its respective products
 
 def home(request):
-    products = None 
-    categories = Category.get_all_categories()
-    cid = request.GET.get('category')
-    if cid:
-        products = Product.get_products_by_id(cid)
+    if request.method == 'GET':
+        products = None 
+        categories = Category.get_all_categories()
+        cid = request.GET.get('category')
+        if cid:
+            products = Product.get_products_by_id(cid)
+        else:
+            products = Product.get_all_products()
+        data = {
+            'products': products,
+            'categories': categories,
+        }
+        return render(request, 'store/home.html', data)
     else:
-        products = Product.get_all_products()
-    data = {
-        'products': products,
-        'categories': categories,
-    }
-    return render(request, 'store/home.html', data)
+        product = request.POST.get('product')
+        remove = request.POST.get('remove')
+        print('email -> ',request.session.get('customer_email'))
+        print('products -> ',product)
+        cart = request.session.get('cart')
+        if cart: # () if cart is presesnt
+            quantity = cart.get(product) # get the quantity that user wants to buy
+            if quantity: # (.) if the user has added that item into the cart
+                if remove: # (..) check if user want to remove
+                    if quantity <= 1: # (...) if product quantity less than 1 then remove item from cart
+                        cart.pop(product) # else the quantity would be zero but it would still remain in cart
+                    else: # (...) else just subtract the quantity by 1
+                        cart[product] = quantity-1
+                else: # (..) else the user wants to add
+                    cart[product] = quantity+1
+            else: # (.) else not then initialize the item by 1
+                cart[product] = 1
+        else: # () else make the user a new cart
+            cart = {}
+            cart[product] = 1
+        request.session['cart'] = cart
+        print('cart ->',cart)
+        return redirect('store-home')
 
 def register(request):
     if request.method == 'GET':
@@ -63,7 +88,7 @@ def register(request):
         if not em:
             customer.password = make_password(customer.password)
             customer.register()
-            return redirect('store-home')
+            return redirect('login')
         else:
             data = {
                 'error': em,
@@ -82,6 +107,8 @@ def login(request):
         if customer:
             flag = check_password(password, customer.password)
             if flag:
+                request.session['customer_id'] = customer.id
+                request.session['customer_email'] = customer.email
                 return redirect('store-home')
             else:
                 error_msg = 'Invalid password for the mentioned EmailID'
